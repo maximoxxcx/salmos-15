10584cb4feb276fb7cd85f5dfcf6c73f0a10a236
maximoxxcx <gamerisaac72@gmail.com>
2026-01-16 22:12:29 -0300

    Update script.js

--- dist/script.js
+++ dist/script.js
@@ -4,21 +4,16 @@ const video = document.getElementById("video");
 const music = document.getElementById("bg-music");
 
 function iniciar() {
-  // esconder tela inicial
   startScreen.style.display = "none";
 
-  // mostrar vídeo
-  video.style.display = "block";
+    video.style.display = "block";
 
-  // tocar vídeo e música
-  video.play().catch(err => console.log("Erro vídeo:", err));
+    video.play().catch(err => console.log("Erro vídeo:", err));
   music.play().catch(err => console.log("Erro áudio:", err));
 }
 
-// clique
 pressStart.addEventListener("click", iniciar);
 
-// teclado (enter ou espaço)
 document.addEventListener("keydown", (e) => {
   if (e.key === "Enter" || e.key === " ") {
     iniciar();
